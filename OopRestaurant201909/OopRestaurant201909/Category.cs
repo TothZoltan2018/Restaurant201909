c237a1e9e75855b4a518c71877b59fb4d2734aba
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2019-09-18 15:54:17 +0200

    #3:Category letrehozasa

---
+++ OopRestaurant201909/OopRestaurant201909/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace OopRestaurant201909
+{
+    /// <summary>
+    /// Az etelek kategoriajat tartalmazo osztaly
+    /// </summary>
+    public class Category
+    {   
+        /// <summary>
+        /// Az Id nevbol a CodeFirst tudja, hogy ez majd primary key lesz
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Az etel kategoria neve
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
