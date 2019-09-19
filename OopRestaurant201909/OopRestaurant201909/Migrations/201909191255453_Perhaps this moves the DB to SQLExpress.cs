4d822ba04317615d6fdb11f51798c1eae83cf894
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2019-09-19 22:06:16 +0200

    #2.#3: Az adatbazist atkoltoztettuk a localdb-rol a .\sqlserver-re.  Nem ment elsore, az sqlexpress-t hozza kellett adni a \server explorer-ben

---
+++ OopRestaurant201909/OopRestaurant201909/Migrations/201909191255453_Perhaps this moves the DB to SQLExpress.cs
@@ -0,0 +1,16 @@
+namespace OopRestaurant201909.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PerhapsthismovestheDBtoSQLExpress : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
